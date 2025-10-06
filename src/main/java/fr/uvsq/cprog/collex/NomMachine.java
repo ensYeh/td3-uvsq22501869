b572200b2e6c36454e9d76798dc3fce5d7b15352
uvsq22501869 <amayas.mokhtari@ens.uvsq.fr>
2025-10-06 12:24:59 +0200

    Implémentation de la classe NomMachine.

--- src/main/java/fr/uvsq/cprog/collex/NomMachine.java
+++ src/main/java/fr/uvsq/cprog/collex/NomMachine.java
@@ -1,5 +1,69 @@
 package fr.uvsq.cprog.collex;
 
-public class NomMachine {
+import java.util.Objects;
 
+public class NomMachine {
+  private final String nomComplet;
+  private final String nomMachine;
+  private final String nomDomaine;
+
+  public NomMachine(String nomComplet) {
+    if (!estValide(nomComplet)) {
+      throw new IllegalArgumentException("Nom complet invalide : " + nomComplet);
+    }
+
+    this.nomComplet = nomComplet;
+    int index = nomComplet.indexOf('.');
+    this.nomMachine = nomComplet.substring(0, index);
+    this.nomDomaine = nomComplet.substring(index + 1);
+  }
+
+  public static boolean estValide(String nom) {
+    if (nom == null || nom.isBlank()) {
+      return false;
+    }
+
+    int index = nom.indexOf('.');
+    if (index <= 0 || index == nom.length() - 1) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public String getNomComplet() {
+    return nomComplet;
+  }
+
+  public String getNomMachine() {
+    return nomMachine;
+  }
+
+  public String getNomDomaine() {
+    return nomDomaine;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+
+    if (!(o instanceof NomMachine)) {
+      return false;
+    }
+
+    NomMachine machine = (NomMachine) o;
+    return Objects.equals(nomComplet, machine.nomComplet);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(nomComplet);
+  }
+
+  @Override
+  public String toString() {
+    return nomComplet;
+  }
 }
